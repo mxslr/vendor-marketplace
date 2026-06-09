b18436c4e4a0f549405054d2360f2c16e600b5f0
fahdaja <fahdalbantani02@gmai.com>
2026-06-09 22:19:33 +0700

    fix bug in finance module

--- src/finance/finance.module.ts
+++ src/finance/finance.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { FinanceController } from './finance.controller';
 import { FinanceService } from './finance.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [FinanceController],
   providers: [FinanceService],
   exports: [FinanceService],
